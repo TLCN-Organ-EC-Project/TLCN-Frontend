74c7bc91ab79aa0c90c06042677365befe593857
Kem <tuetran25112002@gmail.com>
2023-09-20 14:10:27 +0700

    update Skeleton-blog

---
+++ frontend/src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./skeleton.css";
+
+
+export default function Skeleton({ type }) {
+  const COUNTER = 3;
+  const FeedSkeleton = () => (
+    <div className="postSk">
+      <div className="postSkImg"></div>
+      <div className="postSkInfo">
+        <div className="postSkDetail">
+          <div className="postSkTitle"></div>
+          <div className="postSkauthor"></div>
+          <div className="postSkText"></div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const TopSkeleton = () => (
+    <div className="topSk">
+      <div className="topSkIcon"></div>
+      <div className="topSkIcon"></div>
+      <div className="topSkIcon"></div>
+      <div className="topSkImg"></div>
+    </div>
+  );
+
+  const MenuSkeleton = () => (
+    <div className="menuSk">
+      <div className="menuSkItem"></div>
+      <div className="menuSkItem"></div>
+      <div className="menuSkItem"></div>
+      <div className="menuSkItem"></div>
+    </div>
+  );
+
+  
+
+  const CustomLoading = () => (
+    <div className="custom">
+      <div className="balls">
+        <div className="ball ball1"></div>
+        <div className="ball ball2"></div>
+        <div className="ball ball3"></div>
+      </div>
+      <span className="customText">Loading...</span>
+    </div>
+  );
+
+  if (type === "feed") return Array(COUNTER).fill(<FeedSkeleton />);
+  if (type === "top") return <TopSkeleton />;
+  if (type === "menu") return <MenuSkeleton />;
+  if (type === "custom") return <CustomLoading />;
+}
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import {blogs} from '../ultils/contants'
 import { BlogItem } from '../components'
-import Masonry from 'react-masonry-css'
+import Skeleton from "../components/Skeleton/Skeleton";
 
 
 const Blog = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect (()=>{
+    setTimeout(()=>{
+      setIsLoading(false)
+    },3*1000)
+  },[])
   return (
     <div className='w-main h-full'>
+      {
+        isLoading ? 
         <div className='grid grid-cols-3 justify-center gap-4 pl-4'>
-           {blogs.map((el)=>(
-                <BlogItem el={el}/>
-           ))}
+           <Skeleton type="feed"/> 
         </div>
+        :
+        (
+          <div className='grid grid-cols-3 justify-center gap-4 pl-4'>
+             {blogs.map((el)=>(
+                 <BlogItem el={el}/>
+             ))}
+         </div>
+        )}
     </div>
   )
 }
