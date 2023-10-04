a815edceec36ebbc0a3f5f6be4361372648f68ec
Kem <tuetran25112002@gmail.com>
2023-10-04 23:01:37 +0700

    update grid

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -6,12 +6,12 @@ import { useNavigate } from 'react-router-dom'
 const Product = ({productData}) => {
   const navigate=useNavigate();
   return (
-    <div className='w-main text-base'>
+    <div className='w-[450px] h-[450px]'>
         <div 
-            className='p-[15px] flex flex-col items-center gap-3'
+            className='p-[15px] flex flex-col text-center justify-center items-center gap-3'
             onClick={()=>navigate(`/${productData?.id}/${productData?.product_name}`)}
             >
-            <img src={productData?.thumb} className=' top-[20px] left-[200px] w-[33%] object-cover rounded-lg '/>
+            <img src={productData?.thumb} className=' top-[20px] left-[200px] w-[300px] h-[300px] object-cover rounded-lg '/>
             <h3 className='text-'>{productData?.product_name}</h3>
             <div>{`${formatNumber(productData?.price)}đ`}</div>
         </div>  
