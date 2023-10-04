7a2e1ca4101eb6aabeff6c7f3c6b6d6274e6652c
Kem <tuetran25112002@gmail.com>
2023-10-05 02:35:03 +0700

    skeleton products up

--- frontend/src/components/Skeleton/Skeleton.jsx
+++ frontend/src/components/Skeleton/Skeleton.jsx
@@ -3,7 +3,8 @@ import "./skeleton.css";
 
 
 export default function Skeleton({ type }) {
-  const COUNTER = 3;
+  const COUNTER = 10;
+  const COUNTERPOST=3;
   const FeedSkeleton = () => (
     <div className="postSk">
       <div className="postSkImg"></div>
@@ -18,39 +19,30 @@ export default function Skeleton({ type }) {
     </div>
   );
 
-  const TopSkeleton = () => (
+  const ProductsSkeleton = () => (
     <div className="topSk">
       <div className="topSkIcon"></div>
-      <div className="topSkIcon"></div>
-      <div className="topSkIcon"></div>
-      <div className="topSkImg"></div>
+      <div className="topSkTitle"></div>
+      <div className="topSkPrice"></div>
     </div>
   );
 
-  const MenuSkeleton = () => (
-    <div className="menuSk">
-      <div className="menuSkItem"></div>
-      <div className="menuSkItem"></div>
-      <div className="menuSkItem"></div>
-      <div className="menuSkItem"></div>
-    </div>
-  );
-
-  
-
-  const CustomLoading = () => (
-    <div className="custom">
-      <div className="balls">
-        <div className="ball ball1"></div>
-        <div className="ball ball2"></div>
-        <div className="ball ball3"></div>
+  const Product = () => (
+    <div class="card">
+    <div class="header">
+      <div class="img"></div>
+      <div class="details">
+        <span class="name"></span>
+        <span class="about"></span>
       </div>
-      <span className="customText">Loading...</span>
     </div>
-  );
+  </div>
 
-  if (type === "feed") return Array(COUNTER).fill(<FeedSkeleton />);
-  if (type === "top") return <TopSkeleton />;
-  if (type === "menu") return <MenuSkeleton />;
-  if (type === "custom") return <CustomLoading />;
+  )
+
+
+  if (type === "feed") return Array(COUNTERPOST).fill(<FeedSkeleton />);
+  if (type === "test") return Array(COUNTER).fill(<Product />);
+  if (type === "product") return Array(COUNTER).fill(<ProductsSkeleton />);
+
 }
